32f3408875d27fe98249620596dc3df1cde10937
Quentin Boyer <qbsecond@gmail.com>
2016-12-06 20:03:37 +0100

    Ajout d'un test sur une sequence d'image

---
+++ imgSeq/imgSeq.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+import numpy as np
+from PIL import Image, ImageTk
+import time
+	
+def playAnimation():
+	timeSpent = []
+	for k in range(1,269):
+		startTime = time.clock()
+		
+		cIStr = ""
+		if k < 10:
+			cIStr = "00"+str(k)
+		elif k < 100:
+			cIStr = "0"+str(k)
+		else:
+			cIStr = str(k)
+			
+		img = Image.open("image-"+cIStr+".png")
+		img_tk = ImageTk.PhotoImage(img)
+		label.configure(image=img_tk)
+		label.image = img_tk
+		root.update_idletasks()
+		
+		timeSpent.append(time.clock()-startTime)
+		
+		#time.sleep(1/25)
+	print(np.mean(timeSpent))
+	
+root = tk.Tk()
+label = tk.Label(root)
+label.pack()
+img = Image.open("image-001.png")
+btn = tk.Button(root,text="Start",command=playAnimation)
+btn.pack()
+img_tk = ImageTk.PhotoImage(img)
+label.config(image=img_tk)
+
+root.mainloop()
